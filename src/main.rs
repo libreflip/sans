c74e59f60bb303de047d52faaa4af5099f0f1c46
Katharina Fey <kookie@spacekookie.de>
2017-12-03 14:24:28 +0100

    First working example with camera bindings

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,30 @@ mod rest;
 mod state;
 mod util;
 
+extern crate rscam;
+use rscam::{Camera, Config};
+use std::fs;
+use std::io::prelude::*;
 
 fn main() {
+
+    let mut camera = Camera::new("/dev/video0").unwrap();
+
+    camera
+        .start(&Config {
+            interval: (1, 30), // 30 fps.
+            resolution: (1280, 720),
+            format: b"MJPG",
+            ..Default::default()
+        })
+        .unwrap();
+
+    for i in 0..10 {
+        let frame = camera.capture().unwrap();
+        let mut file = fs::File::create(&format!("frame-{}.jpg", i)).unwrap();
+        file.write_all(&frame[..]).unwrap();
+    }
+
 }
 
 
